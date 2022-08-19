0082f90dc423ab4eb4e333920d078bdeb3f4c974
proDream <sushkoos@gmail.com>
2022-08-19 12:11:59 +0400

    Add HomeWork to seminar 1

---
+++ seminar_1/HW_task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int current = 2;
+if (num > 1)
+{
+    while (current <= num)
+    {
+        Console.Write($"{current}");
+        current += 2;
+        if (current <= num) Console.Write(", ");
+    }
+}
+else Console.WriteLine($"Нет последовательности для числа {num}");
