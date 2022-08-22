51d3cf80b0381791f6f819440288a24a0b101581
proDream <sushkoos@gmail.com>
2022-08-22 22:49:19 +0400

    updated tasks

--- seminar_2/task5/Program.cs
+++ seminar_2/task5/Program.cs
@@ -5,6 +5,12 @@ int num2 = int.Parse(Console.ReadLine());
 int sqr1 = num1 * num1;
 int sqr2 = num2 * num2;
 
+// if (sqr1 != num2 && sqr2 != num1) Console.WriteLine("Нет");
+// else Console.WriteLine("Да");
+
+// if (sqr1 == num2 || sqr2 == num1) Console.WriteLine("Да");
+// else Console.WriteLine("Нет");
+
 if (sqr1 == num2) Console.WriteLine("Да");
 else if (sqr2 == num1) Console.WriteLine("Да");
 else Console.WriteLine("Нет");
