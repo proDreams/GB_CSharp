1efc10afe131f1f8a2e074e988c934a0a6202531
proDream <sushkoos@gmail.com>
2022-08-23 14:46:48 +0400

    Add seminar 2 homework

---
+++ seminar_2/HW_task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите день недели: ");
+int day = int.Parse(Console.ReadLine());
+
+if (0 < day && day < 8)
+{
+    if (day == 6 || day == 7) Console.WriteLine("Да");
+    else Console.WriteLine("Нет");
+}
+else Console.WriteLine("Ошибка, такого дня нет в неделе.");
