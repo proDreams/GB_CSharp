e3a08188ec060087a1598511f0f4bd55a5fdee7e
proDream <sushkoos@gmail.com>
2022-09-24 01:52:38 +0400

    Add python version of Seminar 1 tasks

--- seminar_1/HW_task1/Program.cs
+++ seminar_1/HW_task1/Program.cs
@@ -2,6 +2,7 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
 Console.Write("Введите первое число: ");
 int num1 = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
--- seminar_1/task6/Program.cs
+++ seminar_1/task6/Program.cs
@@ -1,6 +1,9 @@
-﻿Console.Write("Введите число: ");
+﻿// 7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.Write("Введите число: ");
 int num = int.Parse(Console.ReadLine());
 int result = num % 10;
-// string num = Console.ReadLine();
-// string result = num.Remove(0, 2);
 Console.WriteLine(result);
